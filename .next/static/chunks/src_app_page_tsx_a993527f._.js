7360e9192b3df211b51ad09148e929a44f9ae03d
mehakbenipal <benipalmehak41@gmail.com>
2025-07-19 10:25:26 +0530

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1752900950792

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_1e19690a._.js",
+  "static/chunks/node_modules_ff4eaed4._.js"
+],
+    source: "dynamic"
+});
